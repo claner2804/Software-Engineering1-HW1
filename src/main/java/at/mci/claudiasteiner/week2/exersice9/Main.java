c16cef83f26dd71cbf0b3f831d85496b10c4d277
claner2804 <claner2804@gmail.com>
2024-11-02 12:10:16 +0100

    .

---
+++ src/main/java/at/mci/claudiasteiner/week2/exersice9/Main.java
@@ -0,0 +1,34 @@
+package at.mci.claudiasteiner.week2.exersice9;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter an integer a: ");
+            a = scanner.nextInt();
+
+            System.out.print("Enter an integer b: ");
+            b = scanner.nextInt();
+
+            if (b == 0){
+                throw new ArithmeticException("can't divide by zero");
+            }
+
+            int result = a/b;
+            System.out.println("the result is " + a + " / " + b + " = " + result);
+
+
+        } catch (InputMismatchException e) {
+            throw new NumberFormatException("what the fuck");
+        }
+
+    }
+}
